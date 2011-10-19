7bd9331af4c1f9963986924a6e38e93cfbdefb5c
Anders Wallin <anders.e.e.wallin@gmail.com>
2011-10-19 19:57:47 +0300

    document Point and Pose

--- src/g2m/point.hpp
+++ src/g2m/point.hpp
@@ -8,27 +8,34 @@
 namespace g2m {
 
 struct Point {
+    /// Create point at (0,0,0)
     Point():x(0),y(0),z(0) {}
+    /// Create point at given coordinates
     Point(double a, double b, double c):x(a),y(b),z(c) {}
+    /// copy-constructor
     Point(const Point& other): x(other.x),y(other.y),z(other.z) {}
+    /// distance to given other Point
     double Distance( Point other ) {
         return sqrt( (other.x-x)*(other.x-x) + (other.y-y)*(other.y-y) + (other.z-z)*(other.z-z) );
     }
-    
+    /// string representation
     std::string str() const {
         std::ostringstream o;
         o << "(" << x << ", " << y << ", " << z << ")";
         return o.str();
     }
+    /// multiply Point with scalar
     Point& operator*=(const double &a) {
         x*=a;
         y*=a;
         z*=a;
         return *this;
     }
+    /// multiply Point with scalar
     Point  operator*(const double &a) const {
         return Point(*this) *= a;
     }
+    /// vector addition
     Point& operator+=( const Point& p) {
         x+=p.x;
         y+=p.y;
@@ -36,32 +43,47 @@ struct Point {
         return *this;
     }
     
+    /// vector addition
     const Point operator+( const Point &p) const {
         return Point(*this) += p;
     }
     
+    /// vector subtraction
     Point& operator-=( const Point& p) {
         x-=p.x;
         y-=p.y;
         z-=p.z;
         return *this;
     }
-    
+    /// vector subtraction
     const Point operator-( const Point &p) const {
         return Point(*this) -= p;
     }
     
 // DATA
-    double x,y,z;
+    /// x-coordinate
+    double x;
+    /// y-coordinate
+    double y;
+    /// z-coordinate
+    double z;
 };
 
+/// a pose is a complete 6-dimensional description of the position and rotation
+/// of an object in 3D space (e.g. a tool). This includes the position of the origin
+/// of the tool (loc) and the direction of the tool axis (dir)
 struct Pose {
     Pose() {}
+    /// Create Pose with given location and direction
+    /// \param a location
+    /// \param b direction
     Pose( Point a, Point b ) {
         loc = a;
         dir = b;
     }
+    /// location
     Point loc;
+    /// direction
     Point dir;
 };
 
